3cc78724c62940bde483ab977577db845dbaa9c4
D-Sluchinski <Derek.Sluchinski@edu.sait.ca>
2025-01-16 17:28:20 -0700

    initialize

--- App.tsx
+++ App.tsx
@@ -1,11 +1,107 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { vacation_location, vacation_price } from './app/vacations';
 
 export default function App() {
+  //typescript is javascript that has types
+
+  type Person ={
+    name: String;
+    age: Number;
+    occupation: String,
+    isEmployed?: Boolean,
+    hobbies:  (String | Number)[];
+    address?: {street: String; city: String; state: String}; //put ? because i dont want to put it in person2
+    vacation_location_value?: String; // question mark just so it's ok to not have
+    vacation_price_value?: String;
+  };
+
+      
+  interface PersonInterface { 
+    name: String;
+    age:Number;
+    occupation: String;
+    isEmployed: Boolean;
+    hobbies: (String | Number)[];
+    address: {street: String; city: String; state: String};
+  };
+
+  
+
+  //*** */
+  // type Person1 ={ 
+  //   name: String,
+  //   age: Number,
+  //   occupation: String,
+  //   hobbies: (String | number)[],
+  //   address: (String)[],
+  // };
+
+
+
+  //const name = "John Doe"; to do type script use tge one below
+  const name: string = "John Doe";
+  let occupation:string = "Software Developer";
+  const age: number = 30;
+  let isEmployed: boolean= true;
+  //array
+  //const hobbies: string[] = ["Reading", "Coding", "Traveling"];
+  //use any for anything or
+  //const hobbies: any[] = ["Reading", "Coding", "Traveling", 4];
+  const hobbies: (string | number)[] = ["Reading", "Coding", "Traveling", 4];
+
+  const address: {street: string; 
+                  city: string; 
+                  state: string} = {
+  street: "123 Main Street",
+  city: "New York",
+  state: "NY", 
+  } as typeof address;
+
+
+
+  //try to NOT use
+  const secondAddress = {
+    street: "123 Main Street",
+    city: "New York",
+    state: "NY", };
+
+
+
+    const person1: PersonInterface ={ 
+    name,
+    age,
+    occupation,
+    isEmployed,
+    hobbies,
+    address,
+  };
+
+  const person2: Person = {name: "Jane Doe", age: 25, occupation:"Nurse", isEmployed: true, hobbies: ["Reading", "drawing"],
+     vacation_location_value: vacation_location("Paris"),
+    vacation_price_value: vacation_price(500, "Paris"),
+     }
+
+  const personlist = [person1, person2];
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.title}>Welcome to my application</Text>
+      <Text>Hello {person2.name}.  You are going to {person2.vacation_location_value}
+        {person2.vacation_price_value},</Text>
+      <Text></Text>
+      
+      <View>
+
+      <TouchableOpacity onPress={()=>{alert("weekend is the best")}}>
+        <Text>Click Me</Text>
+        </TouchableOpacity>
+      </View>
+
+      </View>
+ <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,4 +113,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  
+   title: {
+    fontSize: 24,
+
+
+    },
+  
 });
---
+++ app/vacations.ts
@@ -0,0 +1,15 @@
+
+//use the ():String to know if something is wrong when returning
+export function vacation_location(location: String): String {
+
+    //MAKE SURE USING THE ONE BEFORE "1" FOR THE STRING
+    return `You are going to: ${location}`;
+}
+
+//arrow function
+export const vacation_price = (price: number, location: string): string => {
+    if (location === "Paris") {
+        return `the price of the cacation is: ${price+100}`;
+    }
+    return `the price of the cataion is ${price}`;
+}
